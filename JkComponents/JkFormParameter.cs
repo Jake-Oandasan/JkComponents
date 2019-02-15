af4ca365fa4fb40e6280993ba14d39ff7fb501ce
Jake Oandasan <jake.oandasan@yahoo.com>
2019-02-15 21:50:06 +0800

    Add other properties for JkFormParameters which will be used for auto generation of form parameter control/selection

--- JkComponents/JkFormParameter.cs
+++ JkComponents/JkFormParameter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace JkComponents
 {
@@ -11,5 +12,11 @@ namespace JkComponents
     {
         public String Name { get; set; }
         public String Value { get; set; }
+        public int Index { get; set; }
+        public SqlDbType Type { get; set; }
+        public String ControlName { get; set; } //for assigning of lookup values
+        public bool Visible { get; set; }
+        public String Caption { get; set; }
+        public int Width { get; set; }
     }
 }
