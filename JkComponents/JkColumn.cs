739fc9e243ecf4db3461ba8b1802977150a4295d
Jake Oandasan <jake.oandasan@yahoo.com>
2019-02-17 21:59:36 +0800

    Enhance DataType property of JkColumn from a user defined to SqlDbType

--- JkComponents/JkColumn.cs
+++ JkComponents/JkColumn.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace JkComponents
 {
     [Serializable]
     public class JkColumn
     {
-        public enum ColumnDataTypes { dtString, dtInteger, dtBoolean, dtDouble, dtDateTime };
         public String Name { get; set; }
         public String Caption { get; set; }
-        public ColumnDataTypes DataType { get; set; }
+        public SqlDbType DataType { get; set; }
         public int Width { get; set; }
         public enum ColumnFooterTypes { ftNone, ftCount, ftSum, ftAvg, ftMin, ftMax };
         public ColumnFooterTypes FooterType { get; set; }
